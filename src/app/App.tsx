c4465457c9462d2b08637e960bd4a24d63385c63
mishok2004 <mishok2004@mail.ru>
2024-01-31 21:39:58 +0300

    config appRouter, add Navbar

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
+
+const App = () => {
+   const { theme, toggleTheme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
+        </div>
+    );
+};
+
+export default App;
