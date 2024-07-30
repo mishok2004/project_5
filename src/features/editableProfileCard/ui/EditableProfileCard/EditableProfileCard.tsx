50043082eaac1ece0d5ef981977d90d0be9de5fd
mishok2004 <mishok2004@mail.ru>
2024-07-30 23:48:01 +0300

    redesign text, card, popups, header

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -12,7 +12,7 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { VStack } from '@/shared/ui/deprecated/Stack';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 import { ValidateProfileError } from '../../model/consts/consts';
 import { getProfileForm } from '../../model/selectors/getProfileForm/getProfileForm';
 import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
